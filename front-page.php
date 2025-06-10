3d1487db22d81011cd3714e49d5d29e385717f56
cambreau <breau.camille76@gmail.com>
2025-06-10 10:02:35 -0400

    Section populaire ajout

--- front-page.php
+++ front-page.php
@@ -93,30 +93,31 @@
       </section>
       <section class="populaire">
         <h2 class="populaire__titre">Nos destinations populaires</h2>
+        <div class="populaire__conteneur-articles">
         <?php if (have_posts()): 
           while (have_posts()): the_post(); ?>  
-          <article class="populaire__article">
-          <!-- Boucle WordPress pour afficher les posts -->  
-           <?php
-           the_post_thumbnail('thumbnail');
-  
-           ?>
-              <h3 class="populaire__article-titre"><?php the_title(); ?></h3>
-              <!-- Permet d'afficher le titre principal du post -->
-              <div class="populaire__article-contenu">
-                <?php 
-                $lien = "<a href=" . get_permalink() . ">Suite</a>";
-                // Permet de recuperer le lien du post
-                // Sur WP tableau de bord > Reglages > Permaliens = "Nom de l'article" alors le lien est de la forme : https://www.monsite.com/nom-de-l-article/
-                echo wp_trim_words(get_the_excerpt(), 10,$lien);
-                // Affiche un extrait/resume du post, utile pour les articles de blog et on choisit le nombre de mots max. Par defaut, c'est 55 mots.
-                // the_content();
-                 ?>
-                <!-- Permet d'afficher l'ensemble du contenu du post (article ou image) -->
-              </div>
+            <article class="populaire__article">
+            <!-- Boucle WordPress pour afficher les posts -->  
+            <?php
+            the_post_thumbnail('thumbnail');
+    
+            ?>
+                <h3 class="populaire__article-titre"><?php the_title(); ?></h3>
+                <!-- Permet d'afficher le titre principal du post -->
+                <div class="populaire__article-contenu">
+                  <?php 
+                  $lien = "<a href=" . get_permalink() . ">Suite</a>";
+                  // Permet de recuperer le lien du post
+                  // Sur WP tableau de bord > Reglages > Permaliens = "Nom de l'article" alors le lien est de la forme : https://www.monsite.com/nom-de-l-article/
+                  echo wp_trim_words(get_the_excerpt(), 10,$lien);
+                  // Affiche un extrait/resume du post, utile pour les articles de blog et on choisit le nombre de mots max. Par defaut, c'est 55 mots.
+                  // the_content();
+                  ?>
+                  <!-- Permet d'afficher l'ensemble du contenu du post (article ou image) -->
             </article>
           <?php endwhile; ?>
-          <?php endif; ?>
+        <?php endif; ?>
+        </div>
       </section>
    </main>
    <!-- Recupere le footer -->
--- index.php
+++ index.php
@@ -10,22 +10,25 @@
       <!-- C'est une trace pour debugage seulement, a retirer sur la version finale -->
       <section class="populaire">
         <h2 class="populaire__titre">Nos destinations populaires</h2>
-        <?php if (have_posts()): 
-          while (have_posts()): the_post(); ?> 
-          <article class="populaire__article">
-          <!-- Boucle WordPress pour afficher les posts -->  
-           <?php
-           the_post_thumbnail('thumbnail');
-           ?>
-              <h3 class="populaire__article-titre"><?php the_title(); ?></h3>
-              <!-- Permet d'afficher le titre principal du post -->
-              <div class="populaire__article-contenu">
-                <?php the_content(); ?>
-                <!-- Permet d'afficher l'ensemble du contenu du post (article ou image) -->
-              </div>
+        <div class="populaire__conteneur-articles">
+          <?php if (have_posts()): 
+            while (have_posts()): the_post(); 
+          ?> 
+            <article class="populaire__article">
+                <!-- Boucle WordPress pour afficher les posts -->  
+                <?php
+                the_post_thumbnail('thumbnail');
+                ?>
+                <h3 class="populaire__article-titre"><?php the_title(); ?></h3>
+                <!-- Permet d'afficher le titre principal du post -->
+                <div class="populaire__article-contenu">
+                  <?php the_content(); ?>
+                  <!-- Permet d'afficher l'ensemble du contenu du post (article ou image) -->
+                </div>
             </article>
           <?php endwhile; ?>
-          <?php endif; ?>
+         <?php endif; ?>
+        </div>
       </section>
    </main>
    <!-- Recupere le footer -->
