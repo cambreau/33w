fed6c05c9797981ef2a6124035820fa8376549b7
cambreau <breau.camille76@gmail.com>
2025-08-20 20:48:32 -0400

    Animation carrousel

--- script/carrousel.js
+++ script/carrousel.js
@@ -2,18 +2,38 @@
   const carrousels = document.querySelectorAll(".carrousel");
   const radios = document.querySelectorAll(".carrousel__radio");
 
+  // Debug
+  // console.log(carrousels.length);
+  // console.log(radios.length);
+
   let currentIndex = 0;
   let autoPlayInterval;
 
   // Fonction pour passer à l'image suivante
   function nextImage() {
-    currentIndex = (currentIndex + 1) % carrousels.length;
-    showImage(currentIndex);
+    // Debug :
+    // console.log( currentIndex, carrousels.length);
+
+    // Calculer le prochain index
+    let nextIndex = (currentIndex + 1) % carrousels.length;
+
+    // Debug
+    // console.log(nextIndex);
+
+    showImage(nextIndex);
   }
 
-  // Fonction pour afficher une image spécifique
+  // Fonction pour afficher une image
   function showImage(index) {
-    initialise_carrousel();
+    // console.log(index);
+    // console.log(carrousels.length);
+
+    // Masquer l'image actuelle avec fade out
+    if (carrousels[currentIndex]) {
+      carrousels[currentIndex].style.opacity = 0;
+    }
+
+    // Afficher la nouvelle image
     carrousels[index].style.opacity = 1;
 
     // Cocher le bon bouton radio
@@ -21,6 +41,7 @@
       radios[index].checked = true;
     }
 
+    // Mettre à jour l'index actuel
     currentIndex = index;
   }
 
